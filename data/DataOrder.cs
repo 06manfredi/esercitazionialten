b82d3bfafecdf27125fc35ce99235db826f8c779
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-15 17:52:39 +0100

    Add DataOrder class to retrieve orders

---
+++ data/DataOrder.cs
@@ -0,0 +1,19 @@
+namespace tirocinio;
+
+public static class DataOrder
+{
+    public static List<Order> Get()
+    {
+        var mario = new Customer("Mario");
+        var luigi = new Customer("Luigi");
+
+        var ordini = new List<Order>
+        {
+            new("Penne", 2, 1.5, mario),
+            new("Cartucce", 4, 0.5, luigi),
+            new("Tappi", 5, 1, mario)
+        };
+
+        return ordini;
+    }
+}

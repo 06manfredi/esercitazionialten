db9ba153e101d7d44211e45a4e0a7da27ee669dc
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-15 17:18:04 +0100

    Refactor code: Removed classes (now implemented in models)

--- es1.cs
+++ es1.cs
@@ -1,36 +1,5 @@
 namespace tirocinio;
 
-public class Customer
-{
-    public string name;
-
-    public Customer(string name)
-    {
-        this.name = name;
-    }
-}
-
-public class Order
-{
-    public string name;
-    public int quantity;
-    public double price;
-    public Customer customer;
-
-    public Order(string name, int quantity, double price, Customer customer)
-    {
-        this.name = name;
-        this.quantity = quantity;
-        this.price = price;
-        this.customer = customer;
-    }
-
-    override public string ToString()
-    {
-        return $"{customer.name}\t|\t{name} qta{quantity} ${price}";
-    }
-}
-
 public class Esercizio1
 {
     public static void Run()

e589355d661dc01381fcbbe4b0eff6988018928a
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-19 17:00:30 +0100

    Refactor Order class properties

--- models/Order.cs
+++ models/Order.cs
@@ -1,23 +1,14 @@
 namespace tirocinio.Models;
 
 using Microsoft.EntityFrameworkCore;
-using System;
 
 public class Order
 {
     public int OrderId { get; set; }
-    public string name { get; set; }
-    public int quantity { get; set; }
-    public double price { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
     //public Customer customer { get; set; }
-
-
-
-
-    override public string ToString()
-    {
-        return $"\t|\t{name} qta{quantity} ${price}";
-    }
 }
 
 
@@ -25,14 +16,8 @@ public class OrderContext : DbContext
 {
     public DbSet<Order> Orders { get; set; }
 
-
-    public string DbPath { get; }
-
-
-    // The following configures EF to create a Sqlite database file in the
-    // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     { 
-        optionsBuilder.UseSqlServer(@"Server=1C84KS3-A081\SQLEXPRESS\mssqllocaldb;Database=Test"); 
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=Test;TrustServerCertificate=True;Integrated Security=SSPI;"); 
     }
 }

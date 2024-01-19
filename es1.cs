8ccea93b94c2f2bb1a67f8cce9213c675735ae94
Lorenzo Gugliara <guguxd@hotmail.it>
2024-01-19 17:00:39 +0100

    Refactor code: Remove unused code in Esercizio1 class

--- es1.cs
+++ es1.cs
@@ -1,7 +1,6 @@
-namespace tirocinio;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-using tirocinio.Data;
-using tirocinio.Models;
+namespace tirocinio;
 
 public class Esercizio1
 {
@@ -10,19 +9,6 @@ public class Esercizio1
         Task.Run(async () =>
         {
             Console.WriteLine("ESERCIZIO 1");
-            var ordini = await DataOrder.Get();
-
-            var stampa = string.Join(
-                Environment.NewLine,
-                ordini
-                    .GroupBy(o => o.name)
-                    .OrderByDescending(o => o.Sum(x => x.quantity))
-                    .Select(g => g.OrderByDescending(o => o.quantity))
-                    .Select(g => g.Select(o => o.ToString()))
-                    .Select(g => string.Join(Environment.NewLine, g))
-            );
-
-            Console.WriteLine(stampa);
         }).Wait();
     }
 }
